01f918fd183f652c50e12132291496d189888739
Konstantin <smykovkn@gmail.com>
2022-04-16 12:49:06 +0700

    Add the second task; Add function printThreeWords;

--- src/main/java/ru/gb/smykov/homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/smykov/homework1/HomeWorkApp.java
@@ -2,6 +2,12 @@ package ru.gb.smykov.homework1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
+        printThreeWords();
+    }
 
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
     }
 }
