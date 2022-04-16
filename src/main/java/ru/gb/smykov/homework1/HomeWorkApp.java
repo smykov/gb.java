af75fa9065da8533457d4df78ed8d36103ccff3c
Konstantin <smykovkn@gmail.com>
2022-04-16 13:01:33 +0700

    Add the fourth task; Add function printColor;

--- src/main/java/ru/gb/smykov/homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/smykov/homework1/HomeWorkApp.java
@@ -4,6 +4,7 @@ public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
+        printColor();
     }
 
     public static void printThreeWords() {
@@ -19,6 +20,18 @@ public class HomeWorkApp {
             System.out.println("Сумма положительная");
         } else {
             System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = 3;
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
         }
     }
 }
