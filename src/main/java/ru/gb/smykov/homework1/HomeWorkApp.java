bb61464ebe190f52a9249570a4c4805aea9ffb85
Konstantin <smykovkn@gmail.com>
2022-04-16 12:54:59 +0700

    Add the third task; Add function checkSumSign;

--- src/main/java/ru/gb/smykov/homework1/HomeWorkApp.java
+++ src/main/java/ru/gb/smykov/homework1/HomeWorkApp.java
@@ -3,11 +3,22 @@ package ru.gb.smykov.homework1;
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = -5, b = 3, sum;
+        sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 }
