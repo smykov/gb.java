408eb1ec3030b56d7f6772dc908177d2911c96f0
Konstantin <smykovkn@gmail.com>
2022-04-17 08:44:16 +0700

    Add the first task; Created a function that checks the sum of numbers for positive or negative.

--- src/main/java/ru/gb/smykov/Main.java
+++ src/main/java/ru/gb/smykov/Main.java
@@ -3,5 +3,18 @@ package ru.gb.smykov;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println(CheckSumNumbers(5, 8));
+    }
+
+    public static boolean CheckSumNumbers(int a, int b) {
+        int min = 10, max = 20;
+        int sum = a + b;
+
+        if (sum >= min && sum <= max) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
