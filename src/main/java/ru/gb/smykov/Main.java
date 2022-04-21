db65ff9526bcff2a484ecd43a84fbf287e751d30
Konstantin <smykovkn@gmail.com>
2022-04-21 19:59:37 +0700

    Added the third task; initialize array and all values less than 6 multiply it by 2

--- src/main/java/ru/gb/smykov/Main.java
+++ src/main/java/ru/gb/smykov/Main.java
@@ -11,6 +11,21 @@ public class Main {
 
         initArraySize100();
 
+        initArrayAndValuesLess6MultiplyBy2();
+
+
+    }
+
+    private static void initArrayAndValuesLess6MultiplyBy2() {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
     }
 
     private static void initArraySize100() {
