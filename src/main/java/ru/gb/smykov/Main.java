ca15f1b8420f1270023c181a00c14aa29437e9ae
Konstantin <smykovkn@gmail.com>
2022-04-21 19:45:48 +0700

    Added the first task; Created a function that initializes an array and reverses the values in it.

--- src/main/java/ru/gb/smykov/Main.java
+++ src/main/java/ru/gb/smykov/Main.java
@@ -1,7 +1,32 @@
 package ru.gb.smykov;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        initArrayAndReversesValues();
+    }
+
+    private static void initArrayAndReversesValues() {
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = new Random().nextInt(2);
+        }
+
+        System.out.println(Arrays.toString(array));
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1) {
+                array[i] = 0;
+            } else {
+                array[i] = 1;
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
     }
 }
