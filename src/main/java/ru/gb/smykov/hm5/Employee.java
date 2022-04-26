47c0deefd2372064d825e47ba49d3f3b6864827a
Konstantin <smykovkn@gmail.com>
2022-04-26 21:09:54 +0700

    Modified the first task; Added getters and setters

--- src/main/java/ru/gb/smykov/hm5/Employee.java
+++ src/main/java/ru/gb/smykov/hm5/Employee.java
@@ -7,5 +7,64 @@ public class Employee {
     String email;
     String phone;
     int salary;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age >= 0 && age <= 150) {
+            this.age = age;
+        }
+    }
+
     int age;
 }
