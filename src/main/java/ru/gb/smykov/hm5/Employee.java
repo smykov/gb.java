f7ee982541cd4629c4bcd68a305831b55950ab3b
Konstantin <smykovkn@gmail.com>
2022-04-26 22:03:57 +0700

    Added the second task; Create constructor

--- src/main/java/ru/gb/smykov/hm5/Employee.java
+++ src/main/java/ru/gb/smykov/hm5/Employee.java
@@ -1,12 +1,42 @@
 package ru.gb.smykov.hm5;
 
 public class Employee {
-    String firstName;
-    String secondName;
-    String position;
-    String email;
-    String phone;
-    int salary;
+    private String firstName;
+    private String secondName;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+
+    public Employee(String firstName, String secondName) {
+        this(firstName, secondName, null);
+    }
+
+    public Employee(String firstName, String secondName, String position) {
+        this(firstName, secondName, position, null);
+    }
+
+    public Employee(String firstName, String secondName, String position, String email) {
+        this(firstName, secondName, position, email, null);
+    }
+
+    public Employee(String firstName, String secondName, String position, String email, String phone) {
+        this(firstName, secondName, position, email, phone, 0);
+    }
+
+    public Employee(String firstName, String secondName, String position, String email, String phone, int salary) {
+        this(firstName, secondName, position, email, phone, salary, 0);
+    }
+
+    public Employee(String firstName, String secondName, String position, String email, String phone, int salary, int age) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        setAge(age);
+    }
 
     public String getFirstName() {
         return firstName;
